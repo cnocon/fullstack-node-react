1bdb52621c05441d8d5b3652dba1a5fa4518d723
Cristin O'Connor <cristin@cristin.io>
2017-11-28 06:55:42 -0600

    Advanced refactor: * Condensed routes require to one line.

--- server/index.js
+++ server/index.js
@@ -1,20 +1,18 @@
-// should really only contain business logic & helper modules
-
 const express = require('express');
-// passport file just has to run, not return anything, so we don't need to assign it to an object here, we can just require the file.
-// const passportConfig = require('./services/passport');
-require('./services/passport');
 
-// this will
-const authRoutes = require('./routes/authRoutes');
+// passport file just has to run, not return anything, so we don't need to assign it to an object here, we can just require the file.
+require('./services/passport');
 
 // this has to be accessible in the authRoutes file, so...
 const app = express();
 
-// here is how we pass app object to authRoutes. In the authRoutes file, we put the two get requests in an arrow function that gets exported, so the routes are registered/exist
-authRoutes(app);
-
-
+/**
+  * Cool refactor!
+  *
+  * 1. When we require the authRoutes file, it returns a FUNCTION because that's what we exported from that file!
+  * 2. So what we did was immediately call the function returned from the require with the proper argument, `app`, for that function. Awesome.
+  */
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
