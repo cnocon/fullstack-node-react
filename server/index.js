ef3b98bc5b2b6c56d99e249a76055c68677cb238
Cristin O'Connor <cristin@cristin.io>
2017-11-28 05:48:35 -0600

    * Successfully making it to the step where we successfully authenticated with that special code (via passport) and are kicked back to localhost:5000. * Made sure that in the Google developer console that we added the specific authorized redirect_uri

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-// you don't have to add .js extension if it's a js file
 const keys = require('./config/keys');
 
 const app = express();
@@ -18,10 +17,6 @@ passport.use(
   }
 ));
 
-// route to initialize oauth process
-// as second arg, tell flow to use passport to kick of authentication flow
-// the 'google' string is something built in to passport strategy, which is a little confusing bc it isn't intuitive where it came from
-// scope is what info/access we want for that user's profile
 app.get(
   '/auth/google',
   passport.authenticate('google', {
@@ -29,5 +24,11 @@ app.get(
   })
 );
 
+// now we need to add a route handler that takes the code from the response from Google when a user grants permission to our app. The user details don't get returned from google UNTIL AFTER WE MAKE A NEW REQUEST WITH THIS SPECIAL CODE.
+// the google passport strategy can handle all that for us
+/* all this really does is grab the user profile info; we aren't doing anything other than hanging on that step*/
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT);
