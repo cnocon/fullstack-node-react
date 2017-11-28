3431332428f63b0b4e7d7632e3f43fdf363f82ac
Cristin O'Connor <cristin@cristin.io>
2017-11-28 04:55:06 -0600

    Added our project in Google developers console and retrieved our clientID and clientSecret for the OAuth process

--- server/index.js
+++ server/index.js
@@ -4,10 +4,20 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
-// creates a new instance of Google Passport Strategy
-// passport.use is saying "passport, I want you to be aware there is a new strategy available; please make use of it so users can use strategy to authenticate in our application"
-// we'll pass in config for how to authenticate users in our application
+// CREATES A NEW INSTANCE OF GOOGLE PASSPORT STRATEGY
+/*passport.use is saying "passport, I want you to be aware there is a new strategy available; please make use of it so users can use strategy to authenticate in our application"
+// we'll pass in config for how to authenticate users in our application: info for Google to identify where the user is coming from using a ClientID and ClientSecret*/
+/* clientID: a public token (anybody can have this, it's ok)
+  clientSecret: a private token; it needs to be secure. DO NOT SHARE. */
+
+/**
+  * CREATE OAUTH PROJECT IN GOOGLE DEVELOPER CONSOLE:
+  * https://console.developers.google.com/ (must use cristinjaneo@gmail.com account,
+    not cristin.io)
+ */
+
 passport.use(new GoogleStrategy());
+
 
 
 const PORT = process.env.PORT || 5000;
