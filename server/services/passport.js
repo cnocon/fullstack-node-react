5fff92c0547a11ddba67fe7ae1999b8d3526733b
Cristin O'Connor <cristin@cristin.io>
2017-12-01 13:47:18 -0600

    Implemented deserializeUser method from Passport, for turning user object into piece of identifying info

--- server/services/passport.js
+++ server/services/passport.js
@@ -6,11 +6,22 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
-// serializeUser is for cookies in Passport; we send it our user object, and done callback
+// serializeUser is for cookies in Passport; we send it our user object, and done callback for after we've turned the user instance into an id
 passport.serializeUser((user, done) => {
   // we tell passport what to use as the identifying info
   // user.id references the Mongo auto-generated unique id.
   done(null, user.id);
+});
+
+// first arg is token stuffed into cookie (the unique identifier we gave it), and 2nd arg is done function for calling after we've turned id back into user instance.
+passport.deserializeUser((userId, done) => {
+  // find user & return done w/that user
+  User.findById(userId)
+    // these queries are always asynchronous so you have to use .then to work with the promise
+    .then(user => {
+      //done always takes error obj, then object
+      done(null, user);
+    });
 });
 
 passport.use(
