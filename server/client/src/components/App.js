cfaaff76864394a8ee6bf113a78794f0add7bf06
Cristin O'Connor <cristin@cristin.io>
2017-12-04 14:06:29 -0600

    Created App Component

---
+++ server/client/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// functional component
+const App = () => {
+  // returns some jsx
+  return (
+    <div>Hi Cristin!</div>
+  );
+};
+
+// export component
+
+export default App;
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -2,4 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render();
+import App from './components/App';
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
